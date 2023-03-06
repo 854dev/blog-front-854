9cb934e0f0c838749d41bbb4c9a9d6de1f832185
854 <jahae854@gmail.com>
2023-03-07 00:56:36 +0900

    add blogPost Components

---
+++ src/components/blogPost/ContentBody.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ContentBody() {
+  return <div>ContentDetail</div>;
+}
+
+export default ContentBody;
---
+++ src/components/blogPost/ContentMeta.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ContentDetail() {
+  return <div>ContentDetail</div>;
+}
+
+export default ContentDetail;
