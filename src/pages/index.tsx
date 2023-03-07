6a594fbb9db98b19d2506b65922dad643a07445c
854 <=>
2023-03-07 18:03:35 +0900

    edit index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
 export default function Home() {
-  return <div>hi hello</div>;
+  return (
+    <div>
+      <div className='hero is-full-screen is-vertical-align is-horizontal-align'>
+        <div>
+          <h1>854 블로그</h1>
+          <p className='text-center'>아래로 스크롤</p>
+        </div>
+      </div>
+
+      <div className='post-list'>
+        <div className='card'>
+          <header>
+            <h4>Card title</h4>
+          </header>
+          <p>A nisi ullam ... cupiditate?</p>
+          <footer className='is-right'>
+            <a className='button primary'>Submit</a>
+            <a className='button'>Cancel</a>
+          </footer>
+        </div>
+      </div>
+
+      <footer className='footer'></footer>
+    </div>
+  );
 }
