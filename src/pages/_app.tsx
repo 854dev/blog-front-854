635d362145e6f8e18793a1373b0948ea729d9ff0
854 <=>
2023-03-07 18:03:24 +0900

    toggle dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,16 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '../styles/index.css';
 import MainLayout from '../layouts/MainLayout';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // prefers-color-scheme 확인
+  useEffect(() => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.add('dark');
+    }
+  }, []);
+
   return (
     <>
       <Head>
