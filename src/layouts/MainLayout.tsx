94b16c645b346ec0f557b388fc58c0ed3ef487a0
854 <=>
2023-03-07 18:03:14 +0900

    edit nav

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,12 +3,26 @@ import ToggleDarkMode from '../components/ToggleDarkMode';
 
 function MainLayout(props: PropsWithChildren) {
   return (
-    <div className='main-wrap'>
-      <main>
-        <ToggleDarkMode></ToggleDarkMode>
-        <section className='container'>{props.children}</section>
-      </main>
-    </div>
+    <>
+      <div className='main-wrap'>
+        <main>
+          <nav className='nav'>
+            <div className='nav-left'>
+              <a className='brand' href='#'>
+                Brand
+              </a>
+              <a>Link 1</a>
+              <a className='active'>Link 2</a>
+            </div>
+            <div className='nav-right'>
+              <ToggleDarkMode></ToggleDarkMode>
+            </div>
+          </nav>
+
+          <section className='container'>{props.children}</section>
+        </main>
+      </div>
+    </>
   );
 }
 
