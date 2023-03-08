8b94a59e2828ac602659949e906648863b78b59f
854 <jahae854@gmail.com>
2023-03-09 04:17:55 +0900

    edit default

--- src/api/axiosDefault.ts
+++ src/api/axiosDefault.ts
@@ -7,7 +7,7 @@ axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 // response에서 data 부분만 가져오도록 설정
-axios.defaults.transformResponse = (data) => JSON.parse(data).data;
+// axios.defaults.transformResponse = (data) => JSON.parse(data).data;
 
 // error를 throw하는 대신 에러 메시지만 출력하도록 설정
 axios.defaults.validateStatus = (status) => status < 500;
