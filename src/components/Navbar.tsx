5bfdbe5b2832079254f99eda4d8eb31bf3cdfc22
854 <jahae854@gmail.com>
2023-03-12 00:34:40 +0900

    edit navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,9 @@ function Navbar() {
   return (
     <nav className='nav'>
       <div className='nav-left'>
-        <Link href='/'>854</Link>
+        <Link href='/'>
+          <h1>854</h1>
+        </Link>
         <Link href='/posts'>posts</Link>
         <Link href='/study'>study</Link>
       </div>
