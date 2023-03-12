f233ed23c15ab6cc31c296fd59801268479a61d9
854 <jahae854@gmail.com>
2023-03-13 02:51:39 +0900

    edit Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className='hero is-vertical-align is-horizontal-align'>
       <div>
         <h1>854 블로그</h1>
-        <p className='text-center'>아래로 스크롤</p>
+        <p className='text-center'>공부하고 기록하기</p>
       </div>
     </div>
   );
