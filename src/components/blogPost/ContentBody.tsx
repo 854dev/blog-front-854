5a0c024d68046c4977b564cd849e3d0eb61e74c5
854 <jahae854@gmail.com>
2023-03-13 02:51:51 +0900

    edit content

--- src/components/blogPost/ContentBody.tsx
+++ src/components/blogPost/ContentBody.tsx
@@ -5,7 +5,7 @@ function ContentBody(props: ContentBody) {
   const { schemaId, schemaValue } = props;
 
   return (
-    <article className='container' key={schemaId} data-schema-id={schemaId}>
+    <article className='container p-3' key={schemaId} data-schema-id={schemaId}>
       <section dangerouslySetInnerHTML={{ __html: schemaValue }}></section>
     </article>
   );
--- src/components/blogPost/ContentMeta.tsx
+++ src/components/blogPost/ContentMeta.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { parseDate } from '../../common/util';
 import { ContentMeta } from '../../types/common';
 
 function ContentMeta(props: ContentMeta) {
   const { title, createdAt, contentId } = props;
 
   return (
-    <div>
+    <div className='px-3'>
       <h1>{title}</h1>
-      <span className='text-light'>{createdAt}</span>
-      <span className='text-light'>{contentId}</span>
+      <span>{parseDate(createdAt, 'yyyy.MM.dd hh:mm')}</span>
     </div>
   );
 }
