9f19105a1de2fe60d083c7e896c8a139e93c3c19
854 <jahae854@gmail.com>
2023-03-13 00:19:59 +0900

    edit ToggleDarkMode

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { toggleDarkMode } from '../common/util';
-
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function ToggleDarkMode() {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <button className='outline' onClick={toggleDarkMode}>
-      toggleDarkMode
+    <button
+      className='outline'
+      onClick={() => {
+        toggleDarkMode();
+        setIsDark(!isDark);
+      }}
+    >
+      <FontAwesomeIcon icon={isDark ? faMoon : faSun} />
     </button>
   );
 }
