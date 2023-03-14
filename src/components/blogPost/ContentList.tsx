8a284c3aa046521319e3f48ef197ea352d7dcf30
854 <jahae854@gmail.com>
2023-03-15 00:48:47 +0900

    edit 애니메이션 적용용

--- src/components/blogPost/ContentList.tsx
+++ src/components/blogPost/ContentList.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ContentMeta } from '../../types/common';
 import ContentCard from './ContentCard';
+import { motion } from 'framer-motion';
 
 function ContentList(props: { contentList: ContentMeta[] }) {
   const { contentList } = props;
+
   return (
     <div className='p-2 content-list row'>
-      {contentList.map((elem) => (
-        <div className='col-12'>
-          <ContentCard key={elem.contentId} {...elem} />
+      {contentList.map((elem, idx) => (
+        <div className='col-12' key={elem.contentId}>
+          <motion.div
+            animate={{ opacity: [0, 1], y: [10, 0] }}
+            transition={{ duration: Math.min(0.2 * idx, 1) }}
+          >
+            <ContentCard key={elem.contentId} {...elem} />
+          </motion.div>
         </div>
       ))}
     </div>
