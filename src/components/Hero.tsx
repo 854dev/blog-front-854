f95eb12901923206d8a0bfc903088d242f1c8452
854 <jahae854@gmail.com>
2023-03-15 01:39:37 +0900

    edit hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import api from '../api/api';
 import { ContentMeta } from '../types/common';
 import ContentList from './blogPost/ContentList';
 import Introduction from './IntroDuction';
 
-function Hero() {
-  const [isPostShow, setisPostShow] = useState(false);
+function Hero(props: { contentList: ContentMeta[] }) {
   const [isLogoMotionEnd, setisLogoMotionEnd] = useState(false);
-  const [recentPost, setRecentPost] = useState<ContentMeta[]>([]);
-
-  const getPosts = async () => {
-    const res = await api.content.getList({ page: 1, limit: 5, contentTypeId: 1 });
-    setRecentPost(res.data.data);
-    setisPostShow(true);
-  };
-
-  useEffect(() => {
-    if (isLogoMotionEnd) {
-      getPosts();
-    }
-  }, [isLogoMotionEnd]);
-
   return (
     <div className='hero is-vertical-align is-horizontal-align'>
       <Introduction
@@ -35,13 +19,12 @@ function Hero() {
 
       <motion.div
         initial={{ opacity: 0, y: 0 }}
-        animate={{ opacity: isPostShow ? 1 : 0, y: 0 }}
+        animate={{ opacity: isLogoMotionEnd ? 1 : 0, y: 0 }}
         transition={{ duration: 0.8 }}
       >
         <div className='py-3'>
           <h3 className='text-center'>Recent Post</h3>
-          <ContentList contentList={recentPost} />
-          <Link href={'/posts'}>more</Link>
+          {isLogoMotionEnd ? <ContentList contentList={props.contentList} /> : null}
         </div>
       </motion.div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
 import Hero from '../components/Hero';
+import api from '../api/api';
 
-export default function Home() {
-  return <Hero />;
+export async function getStaticProps() {
+  const res = await api.content.getList({
+    page: 1,
+    limit: 5,
+    contentTypeId: 1,
+  });
+
+  return {
+    props: { contentList: res.data.data }, // will be passed to the page component as props
+  };
+}
+
+export default function Home({ contentList }) {
+  return <Hero contentList={contentList} />;
 }
