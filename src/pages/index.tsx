7d994e18a5d8bc7226184e1a8cb3433b755a766e
854 <=>
2023-03-15 14:46:52 +0900

    delete Hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Hero from '../components/Hero';
 import api from '../api/api';
+import PageIntro from '../components/PageIntro';
+import ContentList from '../components/blogPost/ContentList';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export async function getStaticProps() {
   const res = await api.content.getList({
@@ -14,5 +18,27 @@ export async function getStaticProps() {
 }
 
 export default function Home({ contentList }) {
-  return <Hero contentList={contentList} />;
+  const [isLogoMotionEnd, setisLogoMotionEnd] = useState(false);
+
+  return (
+    <div>
+      <PageIntro
+        title='854 블로그'
+        content='취미로 하는 블로그. 공부하고 기록하기'
+        onAnimationComplete={() => {
+          setisLogoMotionEnd(true);
+        }}
+      ></PageIntro>
+      <motion.div
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: isLogoMotionEnd ? 1 : 0, y: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className='py-3'>
+          <h3 className='text-center'>Recent Post</h3>
+          {isLogoMotionEnd ? <ContentList contentList={contentList} /> : null}
+        </div>
+      </motion.div>
+    </div>
+  );
 }
