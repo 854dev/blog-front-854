d51d58cc783875666c1631f3003c5c129dc2a800
jahae854 <jahae854@gmail.com>
2023-05-05 17:31:23 +0900

    add mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,31 @@
+import * as React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import '../styles/index.css';
-import MainLayout from '../layouts/MainLayout';
-import { useEffect } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../theme';
+import createEmotionCache from '../createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  // prefers-color-scheme 확인
-  // useEffect(() => {
-  //   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  //     document.body.classList.add('dark');
-  //   }
-  // }, []);
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-  return (
-    <>
-      <Head>
-        <title>NextJS TailwindCSS TypeScript Starter</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </>
-  );
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp;
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
