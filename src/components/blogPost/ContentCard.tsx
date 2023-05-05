cc7a3e960121baff19e3600fbbded3f7774b6783
jahae854 <jahae854@gmail.com>
2023-05-05 18:02:32 +0900

    edit contentCard

--- src/components/blogPost/ContentCard.tsx
+++ src/components/blogPost/ContentCard.tsx
@@ -1,20 +1,39 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React from 'react';
-import { parseDate } from '../../common/util';
-import { ContentMeta } from '../../types/common';
-function ContentCard(props: ContentMeta) {
-  const { title, createdAt, contentId } = props;
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-  const router = useRouter();
+const bull = (
+  <Box component='span' sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}>
+    •
+  </Box>
+);
+
+export default function BasicCard() {
   return (
-    <div className='cursor-pointer card' onClick={() => router.push(`posts/${contentId}`)}>
-      <h4>{title}</h4>
-      <span>{parseDate(createdAt)}</span>
-      <p className='description'></p>
-      <Link href={`posts/${contentId}`}></Link>
-    </div>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant='h5' component='div'>
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
+          adjective
+        </Typography>
+        <Typography variant='body2'>
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small'>Learn More</Button>
+      </CardActions>
+    </Card>
   );
 }
-
-export default ContentCard;
