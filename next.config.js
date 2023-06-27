7813c71ec05fb79882f20e32a36e1e3bc66e5366
854 <jahae854@gmail.com>
2023-06-27 19:54:08 +0900

    edit gitignore, next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  output: 'standalone',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
 };
+
+module.exports = nextConfig;
