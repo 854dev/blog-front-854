3ba38d54fddf21b7e143a5abfc4432f4c770f593
854 <jahae854@gmail.com>
2023-06-27 12:36:13 +0900

    edit : 다크모드 prefers-color-scheme 적용

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -5,6 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function ToggleDarkMode() {
   const [isDark, setIsDark] = useState(false);
 
+  useEffect(() => {
+    const preferDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (preferDark) {
+      toggleDarkMode();
+      setIsDark(true);
+    }
+  }, []);
+
   return (
     <span
       className={`p-1 is-vertical-align`}
