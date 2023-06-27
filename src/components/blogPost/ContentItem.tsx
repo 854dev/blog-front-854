312b5e20cac35fb186c89a57e53c5f0e32a4da27
854 <jahae854@gmail.com>
2023-06-27 15:32:42 +0900

    edit tags

--- src/components/blogPost/ContentItem.tsx
+++ src/components/blogPost/ContentItem.tsx
@@ -4,16 +4,23 @@ import { parseDate } from '../../common/util';
 import { ContentMeta } from '../../types/common';
 
 function ContentItem(props: ContentMeta) {
-  const { title, createdAt, contentId, description } = props;
+  const { title, createdAt, contentId, description, tags } = props;
 
   return (
     <Link href={`posts/${contentId}`}>
       <div className='cursor-pointer content-item'>
-        <p>
+        <p className='title'>
           <b>{title}</b>
         </p>
         <span className='text-dark'>{parseDate(createdAt)}</span>
         <p className='description text-grey'>{description}</p>
+        <div className='tag-list'>
+          {tags.map((elem) => (
+            <span className='tag' key={elem.name}>
+              {elem.name}
+            </span>
+          ))}
+        </div>
       </div>
     </Link>
   );
--- src/components/blogPost/ContentMeta.tsx
+++ src/components/blogPost/ContentMeta.tsx
@@ -3,13 +3,20 @@ import { parseDate } from '../../common/util';
 import { ContentMeta } from '../../types/common';
 
 function ContentMeta(props: ContentMeta) {
-  const { title, createdAt, contentId, description } = props;
+  const { title, createdAt, contentId, description, tags } = props;
 
   return (
     <div>
       <h1>{title}</h1>
       <p>{description}</p>
       <span className='text-grey'>{parseDate(createdAt, 'yyyy.MM.dd hh:mm')}</span>
+      <div className='py-1 tag-list'>
+        {tags.map((elem) => (
+          <span className='tag' key={elem.name}>
+            {elem.name}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
