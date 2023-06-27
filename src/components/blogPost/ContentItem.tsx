f229597ee252e33ab8833a731d1b2544bd131fab
854 <jahae854@gmail.com>
2023-06-27 13:24:44 +0900

    edit description 적용, 클래스 수정

--- src/components/blogPost/ContentItem.tsx
+++ src/components/blogPost/ContentItem.tsx
@@ -4,16 +4,16 @@ import { parseDate } from '../../common/util';
 import { ContentMeta } from '../../types/common';
 
 function ContentItem(props: ContentMeta) {
-  const { title, createdAt, contentId } = props;
+  const { title, createdAt, contentId, description } = props;
 
   return (
     <Link href={`posts/${contentId}`}>
-      <div className='cursor-pointer'>
+      <div className='cursor-pointer content-item'>
         <p>
           <b>{title}</b>
         </p>
-        <span className='text-color-grey'>{parseDate(createdAt)}</span>
-        <p className='description'></p>
+        <span className='text-dark'>{parseDate(createdAt)}</span>
+        <p className='description text-grey'>{description}</p>
       </div>
     </Link>
   );
